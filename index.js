ceb7ef2696e91d1d684a07e513a5f0103ad1cf07
charitraa <stharabi9862187405@gmail.com>
2024-07-26 08:12:17 +0545

    adding soem code

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const Connect_to_MongoDB = require("./src/config/db");
-
 const express = require("express");
-
+const connectDB = require("./src/config/db");
+const userProfileRoutes = require("./src/Router/ProfileRoutes");
+const authRoutes = require("./src/Router/authRoute");
+const profileRoutes = require("./src/Router/ProfileRoutes");
 const app = express();
-const port = 3000; // Define your port
+const port = 5000;
+connectDB();
+app.use(express.json());
 
-Connect_to_MongoDB();
+app.use("/user", userProfileRoutes);
+app.use('/api/auth/', authRoutes);
 
-// Start the server
+// Serve static files from the 'uploads' directory
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+app.use('/api/profile', profileRoutes);
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
